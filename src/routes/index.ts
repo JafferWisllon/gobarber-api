75d3826aed577fc0a68e1ebcb3d8ceffb27b8c43
Jaffer Wisllon <contatojaffer@gmail.com>
2021-07-18 14:12:29 -0300

    criando um agentamento

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import appointmentsRoutes from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack' });
-});
+routes.use('/appointments', appointmentsRoutes);
 
 export default routes;
